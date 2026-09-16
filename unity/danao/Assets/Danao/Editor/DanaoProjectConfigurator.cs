fe29fe2d3f0171ce71b5c1683ad32ae5ccd645d6
chambersbtap <zachary.chambers2@gmail.com>
2026-09-16 23:59:08 +0100

    feat: configure Danao Universal Render Pipeline

--- unity/danao/Assets/Danao/Editor/DanaoProjectConfigurator.cs
+++ unity/danao/Assets/Danao/Editor/DanaoProjectConfigurator.cs
@@ -1,12 +1,18 @@
 #if UNITY_EDITOR
+using System.IO;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace Danao.Editor
 {
     [InitializeOnLoad]
     public static class DanaoProjectConfigurator
     {
+        private const string GeneratedFolder = "Assets/Danao/Generated";
+        private const string PipelineAssetPath = GeneratedFolder + "/DanaoURP.asset";
+
         static DanaoProjectConfigurator() { EditorApplication.delayCall += EnsureProject; }
 
         public static void EnsureProject()
@@ -16,6 +22,30 @@ namespace Danao.Editor
             PlayerSettings.colorSpace = ColorSpace.Linear;
             PlayerSettings.defaultScreenWidth = 1920;
             PlayerSettings.defaultScreenHeight = 1080;
+            EnsureUniversalRenderPipeline();
+        }
+
+        private static void EnsureUniversalRenderPipeline()
+        {
+            if (!AssetDatabase.IsValidFolder(GeneratedFolder))
+            {
+                Directory.CreateDirectory(GeneratedFolder);
+                AssetDatabase.Refresh();
+            }
+
+            var pipeline = AssetDatabase.LoadAssetAtPath<UniversalRenderPipelineAsset>(PipelineAssetPath);
+            if (pipeline == null)
+            {
+                pipeline = UniversalRenderPipelineAsset.Create();
+                pipeline.name = "Danao URP";
+                AssetDatabase.CreateAsset(pipeline, PipelineAssetPath);
+                AssetDatabase.SaveAssets();
+            }
+
+            if (GraphicsSettings.defaultRenderPipeline != pipeline)
+                GraphicsSettings.defaultRenderPipeline = pipeline;
+            if (QualitySettings.renderPipeline != pipeline)
+                QualitySettings.renderPipeline = pipeline;
         }
     }
 }
