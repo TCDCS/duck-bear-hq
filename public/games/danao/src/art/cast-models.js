4fecb85188c9b7ddce2833c82c02fd4c585c8d67
chambersbtap <zachary.chambers2@gmail.com>
2026-09-18 16:10:59 +0100

    Danao lower full cast guard pose

--- public/games/danao/src/art/cast-models.js
+++ public/games/danao/src/art/cast-models.js
@@ -295,7 +295,7 @@ function makeHumanPoseRig(B, results) {
     attackKind = '',
   } = {}) => {
     const heavy = attackKind === 'heavy';
-    const armDrop = 1.04;
+    const armDrop = -1.04;
 
     rotate('upperarm_l',
       -stride * 0.24 - (heavy ? punch * 0.42 : punch * 0.08),
