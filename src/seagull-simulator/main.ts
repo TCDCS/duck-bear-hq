ba5e1fa0c129d3168701ff53ca75c0cc77198082
chambersbtap <zachary.chambers2@gmail.com>
2026-09-19 09:45:14 +0100

    Bump Seagull Simulator alpha version

--- src/seagull-simulator/main.ts
+++ src/seagull-simulator/main.ts
@@ -11,7 +11,7 @@ const FOOD_TYPES=[
 ] as const;
 const WORLD_W=5200;
 const WORLD_H=1800;
-const VERSION='0.1.0-alpha';
+const VERSION='0.2.0-alpha';
 
 type Target = {
   person: Phaser.Physics.Arcade.Sprite;
